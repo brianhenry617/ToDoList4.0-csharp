774ab2a1321d36a172e65b0bb68318193fe7cbeb
Dennise Ortega <dennise.i.ortega@gmail.com>
2018-05-03 15:37:37 -0700

    add types and testing

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -5,37 +5,43 @@ using System;
 
 namespace ToDoList.Controllers
 {
-    public class ItemsController : Controller
+  public class ItemsController : Controller
+  {
+    [HttpGet("/items")]
+    public ActionResult Index()
     {
-      [HttpGet("/items")]
-      public ActionResult Index()
-      {
-          List<Item> allItems = Item.GetAll();
-          return View(allItems);
-      }
-
-      [HttpGet("/items/new")]
-      public ActionResult CreateForm()
-      {
-          return View();
-      }
-      [HttpGet("/categories/{categoryId}/items/new")]
-      public ActionResult CreateForm(int categoryId)
-      {
-        Dictionary<string, object> model = new Dictionary<string, object>();
-        Category category = Category.Find(categoryId);
-        return View(category);
-      }
-      [HttpGet("/categories/{categoryId}/items/{itemId}")]
-       public ActionResult Details(int categoryId, int itemId)
-       {
-          Item item = Item.Find(itemId);
-          Dictionary<string, object> model = new Dictionary<string, object>();
-          Category category = Category.Find(categoryId);
-          model.Add("item", item);
-          model.Add("category", category);
-          return View(item);
-       }
-
+      List<Item> allItems = Item.GetAll();
+      return View(allItems);
     }
+
+    [HttpGet("/items/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+    [HttpGet("/categories/{categoryId}/items/new")]
+    public ActionResult CreateForm(int categoryId)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Category category = Category.Find(categoryId);
+      return View(category);
+    }
+    [HttpGet("/categories/{categoryId}/items/{itemId}")]
+    public ActionResult Details(int categoryId, int itemId)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Item item = Item.Find(itemId);
+      Category category = Category.Find(categoryId);
+      model.Add("item", item);
+      model.Add("category", category);
+      return View(model);
+    }
+    [HttpGet("/items/{id}/update")]
+    public ActionResult Update(int id)
+    {
+      Item thisItem = Item.Find(id);
+      thisItem.Edit(Request.Form["newname"]);
+      return RedirectToAction("Index");
+    }
+  }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 using System;
 namespace ToDoList.Models
 {
@@ -11,11 +12,29 @@ namespace ToDoList.Models
     public Item (string description)
     {
       _description = description;
-      _id = _instances.Count + 1;
+    }
+
+    public override bool Equals(System.Object otherItem)
+    {
+      if (!(otherItem is Item))
+      {
+        return false;
+      }
+      else
+      {
+        Item newItem = (Item) otherItem;
+        bool idEquality = (this.GetId() == newItem.GetId());
+        bool descriptionEquality = (this.GetDescription() == newItem.GetDescription());
+        return (descriptionEquality);
+      }
     }
     public int GetId()
     {
       return _id;
+    }
+    public void SetId(int newId)
+    {
+      _id = newId;
     }
     public string GetDescription()
     {
@@ -34,7 +53,7 @@ namespace ToDoList.Models
 
     public static List<Item> GetAll()
     {
-      List<Item> all Items = new List <Item> {};
+      List<Item> allItems = new List<Item> {};
       MySqlConnection conn = DB.Connection();
       conn.Open();
       MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
@@ -44,7 +63,8 @@ namespace ToDoList.Models
       {
         int itemID = rdr.GetInt32(0);
         string itemDescription = rdr.GetString(1);
-        Item newItem = newItem(itemDescription, itemID);
+        Item newItem = new Item(itemDescription);
+        newItem.SetId(itemID);
         allItems.Add(newItem);
       }
       conn.Close();
@@ -54,17 +74,109 @@ namespace ToDoList.Models
       }
       return allItems;
     }
+    public static void DeleteAll()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM items;";
+
+      cmd.ExecuteNonQuery();
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+    public static Item Find(int id)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM `items` WHERE id = @thisId;";
+
+      MySqlParameter thisId = new MySqlParameter();
+      thisId.ParameterName = "@thisId";
+      thisId.Value = id;
+      cmd.Parameters.Add(thisId);
+
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+
+      int itemId = 0;
+      string itemDescription = "";
+
+      while (rdr.Read())
+      {
+        itemId = rdr.GetInt32(0);
+        itemDescription = rdr.GetString(1);
+      }
+
+      Item foundItem= new Item(itemDescription);
+      foundItem.SetId(itemId);
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return foundItem;
+    }
     public void Save()
     {
-      _instances.Add(this);
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO `items` (`description`) VALUES (@ItemDescription);";
+
+      MySqlParameter description = new MySqlParameter();
+      description.ParameterName = "@ItemDescription";
+      description.Value = _description;
+      cmd.Parameters.Add(description);
+
+      cmd.ExecuteNonQuery();
+      _id = (int)cmd.LastInsertedId;
+
+      // more logic will go here in a moment
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+    public void Edit(string newDescription)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"UPDATE items SET description = @newDescription WHERE id = @searchId;";
+
+      MySqlParameter searchId = new MySqlParameter();
+      searchId.ParameterName = "@searchId";
+      searchId.Value = _id;
+      cmd.Parameters.Add(searchId);
+
+      MySqlParameter description = new MySqlParameter();
+      description.ParameterName = "@newDescription";
+      description.Value = newDescription;
+      cmd.Parameters.Add(description);
+
+      cmd.ExecuteNonQuery();
+      _description = newDescription;
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
     }
     public static void ClearAll()
     {
       _instances.Clear();
-    }
-    public static Item Find(int searchId)
-    {
-      return _instances[searchId-1];
     }
   }
 }
